f97cc2c0f7a2b4cdd1fc3109e1344dbe53943660
arletamaya <arletamayanabila0@gmail.com>
2025-05-23 12:57:38 +0700

    menambahkan ; yang kurang

--- update_cart.php
+++ update_cart.php
@@ -3,7 +3,7 @@ include 'admin/koneksi.php';
 session_start();
 
 if (!isset($_SESSION['id_user'])) {
-    echo "<script>alert('Silakan login terlebih dahulu!'); window.location='login.php'</script>";
+    echo "<script>alert('Silakan login terlebih dahulu!'); window.location='login.php';</script>";
     exit;
 }
 
@@ -12,16 +12,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['qty'])) {
         $jumlah = intval($jumlah);
         if ($jumlah >= 1) {
             // Ambil id_produk dari id_pesanan untuk cek stok
-            $query_produk = "SELECT pr.id_produk, pr.stok FROM tb_pesanan p JOIN tb_produk pr ON p.id_produk = pr.id_produk WHERE p.id_pesanan = '$id_pesanan'";
+            $query_produk = "SELECT pr.id_produk, pr.stok FROM tb_pesanan p JOIN
+            tb_produk pr ON p.id_produk = pr.id_produk WHERE p.id_pesanan =
+            '$id_pesanan'";
             $res = mysqli_query($koneksi, $query_produk);
             $produk = mysqli_fetch_assoc($res);
 
             if ($jumlah <= $produk['stok']) {
                 // Update qty dan total
-                $query_update = "UPDATE tb_pesanan p JOIN tb_produk pr ON p.id_produk = pr.id_produk SET p.qty = '$jumlah', p.total = (pr.harga * $jumlah) WHERE p.id_pesanan = '$id_pesanan'";
+                $query_update = "UPDATE tb_pesanan p
+                                JOIN tb_produk pr ON p.id_produk = pr.id_produk
+                                SET p.qty = '$jumlah', p.total = (pr.harga * $jumlah)
+                                WHERE p.id_pesanan = '$id_pesanan'";
                 mysqli_query($koneksi, $query_update);
             } else {
-                echo "<script>alert('Jumlah melebihi stok!'); window.location='belanja.php'</script>";
+                echo "<script>alert('Jumlah melebihi stok!'); window.location='belanja.php';</script>";
                 exit;
             }
         }
