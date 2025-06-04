0e45eb19722ce4a9c0f73dd236ff1f21641016c5
arletamaya <arletamayanabila0@gmail.com>
2025-06-04 12:03:54 +0700

    index.php

--- admin/index.php
+++ admin/index.php
@@ -231,7 +231,7 @@ if (!isset($_SESSION["status"]) || $_SESSION["status"] !== "admin") {
 
             $result = mysqli_query($koneksi, $query);
             $data = mysqli_fetch_assoc($result);
-            $totalRevenue = isset($data['total_revenue']) ? $data['total_revenue'] : 0;
+            $totalRevenue = $data['total_revenue'] ?? 0;
             ?>
 
             <!-- Revenue Card -->
